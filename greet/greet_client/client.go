41566ac2b4c5cbb456571b7f06f269b2b609153b
steven victor <chikodi543@gmail.com>
2019-09-12 16:09:55 +0100

    Display info from the server

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,16 +1,17 @@
-package main 
+package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+
 	"github.com/victorsteven/go-grpc/greet/greetpb"
 	"google.golang.org/grpc"
-) 	
+)
 
-func main(){
+func main() {
 	fmt.Println("Hello client")
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-
 	if err != nil {
 		log.Fatal("could not connect: %v", err)
 	}
@@ -19,5 +20,24 @@ func main(){
 
 	c := greetpb.NewGreetServiceClient(cc)
 
-	fmt.Printf("Created client: %f", c)
+	doUnary(c)
+
+	// fmt.Printf("Created client: %f", c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "steven",
+			LastName:  "victor",
+		},
+	}
+	// c.Greet(context.Background(), in *greetpb.GreetRequest, opts ...grpc.CallOption)
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,16 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("the greet is invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello sir")
